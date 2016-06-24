e2241e79e2e1074c5349af5a2ed817371b5904d0
dengliwen <denglw@staff.tianya.cn>
2016-06-24 17:20:32 +0800

    create podfile

---
+++ MyTemplateProject/MyTemplateProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTemplateProject
+//
+//  Created by dengliwen on 16/6/24.
+//  Copyright © 2016年 dengliwen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
